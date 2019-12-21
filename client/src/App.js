402464adb8421d42e5807508642e0274dab6aa25
Erik Tenecela <eriktenecela13@gmail.com>
2019-12-21 13:10:46 -0500

    remove-https

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,16 @@ import Thumbnail from "./Component/Thumbnail";
 class App extends Component {
   state = {
     title: [
-      "https://miro.medium.com/max/3840/1*uyH-s_kUofX3jUegQEF2Eg.jpeg",
-      "https://miro.medium.com/max/6880/1*iJMamLWhc3Z_dnHgWa8qEA.png",
-      "https://cdn.dribbble.com/users/542205/screenshots/4423882/rick_and_morthy_colour.jpg",
-      "https://miro.medium.com/max/3840/1*wHA1s55Ac64idQsvo8okbA.jpeg",
-      "https://miro.medium.com/max/650/1*1zFpVtGXL84YYv74cAskmg.jpeg",
-      "https://miro.medium.com/max/3840/1*yQqQaFZsShv8WhQECUZZ1w.jpeg",
-      "https://fsmedia.imgix.net/f6/d0/44/ee/0f37/429e/a534/ef5ac2e50e13/rickmortycufeaturejpg.jpeg?rect=0%2C86%2C1801%2C900&auto=format%2Ccompress&dpr=2&w=650",
-      "https://cdn-images-1.medium.com/max/1600/1*UeOXKrCupfCW4HrUzvnJcA.jpeg",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6RK4TqpqAeFTypydyOdoGMzX6j4UD1BIeXhthGBqt4Fn37Zo0ug&s",
-      "https://miro.medium.com/max/2242/1*A8DASIeuGPPhNhwBvSgxPw.png"
+      "//miro.medium.com/max/3840/1*uyH-s_kUofX3jUegQEF2Eg.jpeg",
+      "//miro.medium.com/max/6880/1*iJMamLWhc3Z_dnHgWa8qEA.png",
+      "//cdn.dribbble.com/users/542205/screenshots/4423882/rick_and_morthy_colour.jpg",
+      "//miro.medium.com/max/3840/1*wHA1s55Ac64idQsvo8okbA.jpeg",
+      "//miro.medium.com/max/650/1*1zFpVtGXL84YYv74cAskmg.jpeg",
+      "//miro.medium.com/max/3840/1*yQqQaFZsShv8WhQECUZZ1w.jpeg",
+      "//fsmedia.imgix.net/f6/d0/44/ee/0f37/429e/a534/ef5ac2e50e13/rickmortycufeaturejpg.jpeg?rect=0%2C86%2C1801%2C900&auto=format%2Ccompress&dpr=2&w=650",
+      "//cdn-images-1.medium.com/max/1600/1*UeOXKrCupfCW4HrUzvnJcA.jpeg",
+      "//encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6RK4TqpqAeFTypydyOdoGMzX6j4UD1BIeXhthGBqt4Fn37Zo0ug&s",
+      "//miro.medium.com/max/2242/1*A8DASIeuGPPhNhwBvSgxPw.png"
     ],
     score: 0,
     topScore: 0
